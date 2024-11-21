82e14b77546d99ed83936822f076928122c48121
krylov72 <qobject1337@gmail.com>
2024-11-21 12:14:50 +0500

    testLab feat: add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CustomCheckboxComponent } from "./components/UI/custom-checkbox/custom-checkbox.component";
+import { CustomButtonComponent } from "./components/UI/custom-button/custom-button.component";
+import { CustomInputComponent } from "./components/UI/custom-input/custom-input.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomCheckboxComponent, CustomButtonComponent, CustomInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'testLabMain';
+
+  
 }
