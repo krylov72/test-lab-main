995a5f5f15d5224ef5acceaf950e59b29e23860d
krylov72 <qobject1337@gmail.com>
2024-11-22 21:01:56 +0500

    fix

--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -16,22 +16,22 @@ interface Cards {
 export class HowItWorksComponent {
   cards: Cards[] = [
     {
-      image: 'assets/images/truck.png',
+      image: 'images/mouse.png',
       title: 'Прочитай задание внимательно',
       text: 'Думаю у тебя не займет это больше двух-трех минут',
     },
     {
-      image: '/assets/images/truck.png',
+      image: 'images/truck.png',
       title: 'Изучи весь макет заранее',
       text: 'Подумай как это будет работать на разных разрешениях и при скролле',
     },
     {
-      image: '/assets/images/secure.png',
+      image: 'images/secure.png',
       title: 'Сделай хорошо',
       text: 'Чтобы мы могли тебе доверить подобные задачи в будущем',
     },
     {
-      image: '/assets/images/money.png',
+      image: 'images/money.png',
       title: 'Получи предложение',
       text: 'Ну тут все просто, не я придумал правила, но думаю так и будет)))',
     },
