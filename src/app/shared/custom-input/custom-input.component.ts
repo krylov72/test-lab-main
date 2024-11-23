4fe4d959de463a80efe813aa668461f0b3054c70
krylov72 <qobject1337@gmail.com>
2024-11-23 21:34:44 +0500

    fix form/input

--- src/app/shared/custom-input/custom-input.component.ts
+++ src/app/shared/custom-input/custom-input.component.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, forwardRef } from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
   imports: [CommonModule, FormsModule],
-
   templateUrl: './custom-input.component.html',
   styleUrl: './custom-input.component.scss',
 })
-export class CustomInputComponent {}
+export class CustomInputComponent {
+  showPlaceholder = false;
+  onFocus() {
+    this.showPlaceholder = true;
+  }
+
+  onBlur() {
+    this.showPlaceholder = false;
+  }
+}
