5c8a63985f999c2fa18e353329a3ee6ad15eabe5
krylov72 <qobject1337@gmail.com>
2024-11-24 20:26:47 +0500

    fix footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { ReviewsPageComponent } from './features/reviews-page/reviews-page.compo
 import { FaqPageComponent } from './features/faq-page/faq-page.component';
 import { InfoPageComponent } from './features/info-page/info-page.component';
 import { FormComponent } from './features/form/form.component';
+import { FooterComponent } from "./features/footer/footer.component";
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,8 @@ import { FormComponent } from './features/form/form.component';
     FaqPageComponent,
     InfoPageComponent,
     FormComponent,
-  ],
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
