0543658c469df1b8497b1f1b10f1990233f96f50
krylov72 <qobject1337@gmail.com>
2024-11-25 00:05:51 +0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,11 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { HowItWorksComponent } from './features/how-it-works/how-it-works.component';
 import { ThirdBlockComponent } from './features/third-block/third-block.component';
-import { ReviewComponent } from './shared/review/review.component';
 import { ReviewsPageComponent } from './features/reviews-page/reviews-page.component';
 import { FaqPageComponent } from './features/faq-page/faq-page.component';
 import { InfoPageComponent } from './features/info-page/info-page.component';
 import { FormComponent } from './features/form/form.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -24,6 +24,7 @@ import { FormComponent } from './features/form/form.component';
     FaqPageComponent,
     InfoPageComponent,
     FormComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
